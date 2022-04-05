c98447ca8b68f1562ce905773812971137f0a19b
Slv97 <Slv97@mail.ru>
2022-04-05 15:07:25 +0300

    A framework has been made for the TodoList Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = React.useState("");
+    const [todos, setTodos] = React.useState([]);
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const addTodo = () => {
+        if (text.trim().length) {
+            setTodos([
+                ...todos,
+                { id: new Date().toISOString(), text, completed: false },
+            ]);
+        }
+        setText("");
+    };
+
+    const removeTodo = (todoId) => {
+        setTodos(todos.filter((todo) => todo.id !== todoId));
+    };
+
+    const toggleTodoComplete = (todoId) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id !== todoId) return todo;
+
+                return {
+                    ...todo,
+                    completed: !todo.completed,
+                };
+            })
+        );
+    };
+    return (
+        <div className="App">
+            <label>
+                <input value={text} type="text" onChange={handleChange} />
+                <button onClick={addTodo}>add</button>
+            </label>
+
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={() => toggleTodoComplete(todo.id)}
+                        />
+                        <span>{todo.text}</span>
+                        <span
+                            className="delete"
+                            onClick={() => removeTodo(todo.id)}
+                        >
+                            &times;
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
